46706ce049ba70c759d413213d0dd6fa785ba23f
matthew <matthewgard1@yahoo.com>
2022-05-27 21:49:59 -0600

    desc

--- pplMachine/src/components/HomepageFeatures/index.js
+++ pplMachine/src/components/HomepageFeatures/index.js
@@ -4,29 +4,26 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'SpinJam',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        spinning jammin creating gif spinners 🌀
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'DEMO',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        ...💥
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'U',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Secret 😎
       </>
     ),
   },
